a6e63b765daeb9011763dedd7c8e4b01d4326938
Flavio Oria <flavio.oriap@gmail.com>
2024-10-21 08:38:59 +0200

    ✨ Add customisizing themes preset and tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,16 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
-export default <Partial<Config>> {
+export default <Partial<Config>>{
   content: [],
   theme: {
     extend: {
-    }
+      colors: {
+        primary: "var(--primary-color)",
+        secondary: "var(--secondary-color)",
+        component: "var(--component-color)",
+        "font-color": "var(--text-color)",
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
